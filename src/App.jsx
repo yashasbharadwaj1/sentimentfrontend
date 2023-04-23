e4974589df6a26dc4403c76247d334c1f4ad09be
Yashas.A.bharadwaj <yashasbharadwaj6@gmail.com>
2023-04-23 17:19:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function App() {
+    return (
+        <div >
+            <h1>My Portfolio</h1>
+
+        </div>
+    );
+}
+
+export default App
